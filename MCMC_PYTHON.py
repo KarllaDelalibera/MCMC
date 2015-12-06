405ca648a77d8ad9d7a4a0146ab19505526c028d
TeoCalvo <teocalo2@gmail.com>
2015-12-06 16:09:49 -0200

    Edicao Teo

--- MCMC_PYTHON.py
+++ MCMC_PYTHON.py
@@ -3,25 +3,24 @@
 ### BIBLIOTECAS
 
 import scipy.special as sps
-import numpy.random
 import numpy as np
 import matplotlib.pyplot as plt
-import pandas
-from pandas import DataFrame
+import pandas as pd
 from scipy.stats import norm
 from scipy.stats import gamma
-import rpy2.robjects as robjects
 from math import exp
+from functools import reduce
+import operator
 
 ########################################
 
 ### DADOS
 
 v=[190,220,240,260]
-dados=numpy.matrix([[7228,7228,7228,8448,9167,9167,9167,9167,10511,10511],[1764,2436,2436,2436,2436,2436,3108,3108,3108,3108],[1175,1175,1521,1569,1617,1665,1665,1713,1761,1953],[600,744,744,744,912,1128,1320,1464,1608,1898]])
+dados=np.matrix([[7228,7228,7228,8448,9167,9167,9167,9167,10511,10511],[1764,2436,2436,2436,2436,2436,3108,3108,3108,3108],[1175,1175,1521,1569,1617,1665,1665,1713,1761,1953],[600,744,744,744,912,1128,1320,1464,1608,1898]])
 trans_dados = dados.T
 
-temp = DataFrame(trans_dados)
+temp = pd.DataFrame(trans_dados)
 
 a = temp.apply(sum)
 
@@ -40,9 +39,6 @@ def fx(a, v, t):
 
 ##### FUNCAO DE VEROSSIMILHANCA
 
-from functools import reduce
-import operator
-
 a = temp.apply(sum)
 
 def L(x, v, t):
@@ -52,32 +48,32 @@ def L(x, v, t):
 
 ### FUNCAO MCMC
 
-def mcmc(N, k, x, v):
-    chute = [[0.1,10,1]]
-    M = chute*N 
-    
+def mcmc(N=1000, k={"t1":100, "t2":1, "t3":1}, x=[], v=[]):
+    chute = {"t1":[0.1],"t2":[10],"t3":[1]}
+    M = chute
+    hiper = {"t1":[0,100],"t2":[0,100],"t3":[0.1,0.1]} #VALORES DOS HIPERPARAMETROS
+
     for i in range(N-1):
-        M[i+1] = M[i]
-        hiper = [[0,100],[0,100],[0.1,0.1]] #VALORES DOS HIPERPARAMETROS
+        for j in M.keys():
+            if j == "t1" or j == "t2": 
+                M[j].append( np.random.normal(loc = M[j][-1]-k[j]/100, scale = k[j], size = 1) )
 
-	for j in [1,2,3]:
-	    if( j == 1 or j == 2): 
-	        M[i+1][j] = numpy.random.normal(loc = M[i][j]-k[j]/100, scale = k[j], size = 1)
-            t1 = norm.pdf(M[i+1][j], loc = hiper[1][j], scale = hiper[2][j]) * L(x, v, M[i+1]) * norm.pdf(M[i][j], loc = M[i+1][j]-k[j]/100, scale = k[j])
-            t2 = norm.pdf(M[i][j], loc = hiper[1][j], scale = hiper[2][j])* L(x, v, M[i])* norm.pdf(M[i+1][j], loc = M[i][j]-k[j]/100, scale = k[j])
+                lista = [ [ M[l][-1] for l in M.keys()] , [ M[l][-1] if l!=j else M[l][-2] for l in M.keys() ] ]
+                    
+                t1 = norm.pdf(M[j][-1], loc = hiper[j][0], scale = hiper[j][1]) * L(x, v, lista[0]) * norm.pdf(M[j][-2], loc = M[j][-1]-k[j]/100, scale = k[j])
+                t2 = norm.pdf(M[j][-2], loc = hiper[j][0], scale = hiper[j][1]) * L(x, v, lista[1]) * norm.pdf(M[j][-1], loc = M[j][-2]-k[j]/100, scale = k[j])
+                
+                teste = (t1/t2)
+            else:
+                M[j].append( np.random.gamma(shape = M[j][-1]*k[j], scale = k[j], size = 1) )
+                t1 =  gamma.pdf(M[j][-1],  shape= hiper[j][0], scale = hiper[j][1]) * L(x, v, lista[0]) * gamma.pdf(M[j][-2], shape = M[j][-1]*k[j], scale = k[j])
+                t2 =  gamma.pdf(M[j][-2],  shape= hiper[j][0], scale = hiper[j][1]) * L(x, v, lista[1]) * gamma.pdf(M[j][-1], shape = M[j][-2]*k[j], scale = k[j])          
+
+                teste = (t1/t2)
+                
+            if (min(1 , teste) < np.random.uniform(how = 0, high = 1, size = 1) ) or (np.isinf(teste)) or (np.isnan(teste)) :  
+                M[j][-1] = M[j][-2]
             
-            teste = (t1/t2)
-        else:
-            M[i+1][j] = numpy.random.gamma(shape = M[i][j]*k[j], scale = k[j], size = 1)
-
-            t1 =  gamma.pdf(M[i+1][j],  shape= hiper[1][j], scale = hiper[2][j]) * L(x, v, M[i+1]) * gamma.pdf(M[i][j], shape = M[i+1][j]*k[j], scale = k[j])
-            t2 =  gamma.pdf(M[i][j],  shape= hiper.rx[1][j], scale = hiper[2][j]) * L(x, v, M[i]) * gamma.pdf(M[i+1][j], shape = M[i][j]*k[j], scale = k[j])            
-
-            teste = (t1/t2)
-            
-        if( (min(1 , teste) < numpy.random.uniform(how = 0, high = 1, size = 1) ) or (numpy.isinf(teste)) or (numpy.isnan(teste)) ):  
-            M[i+1][j] = M[i][j]
-        
     return(M)
 
-    MP = mcmc(N=30000, k=[100,1,1],  x = temp, v = v)
+MP = mcmc(N=30000,  x = temp, v = v)
