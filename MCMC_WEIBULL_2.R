30bb3d736f3b3eab10a5ae4fdc66257a2e07f354
KarllaDelalibera <karlladelalibera@gmail.com>
2015-12-08 11:38:50 -0200

    Update MCMC_WEIBULL_2.R

--- MCMC_WEIBULL_2.R
+++ MCMC_WEIBULL_2.R
@@ -1,4 +1,7 @@
+############################################# MCMC WEIBULL - R ####################################
 
+##########################################
+##### FUNCAO DE DISTRIBUIÇÃO
 fx = function(a, v, t)
 {
 #   t[1] = beta
@@ -7,7 +10,6 @@ fx = function(a, v, t)
 
 	return ( (t[3]/exp(t[2]-(t[1]/v)))*(a/exp(t[2]-(t[1]/v)))^(t[3]-1)*exp(-(a/exp(t[2]-(t[1]/v)))^t[3]) )
 }
-
 
 ##########################################
 ##### FUNCAO DE VEROSSIMILHANCA
@@ -75,38 +77,3 @@ return(list(M=M,taxa))
 
 
 MP = mcmc(N=30000,chute=c(0.1,10,1), hiperv=c(0,100,0,100,0.1,0.1), k=c(100,1,1),  temp, V)
-MC=MP$M
-#MB = MC[15000:30000,]  # BURN-IN SIMPLES
-MB = MC[seq(1,30000,5),] # BURN-IN de 10 em 10 
-
-############################################
-##### ESTATÍSTICA
-
-theta = c(mean(MB[,1]),mean(MB[,2]),mean(MB[,3]))
-
-############################################
-#### VERIFICAÇÃO DA CONVERGENCIA
-
-par(mfrow=c(3,3))
-
-ts.plot(MB[,1],ylab=expression(paste(beta)))
-ts.plot(MB[,2],ylab=expression(paste(alpha)))
-ts.plot(MB[,3],ylab=expression(paste(rho)))
-
-acf(MB[,1],ylab=expression(paste(beta)))
-acf(MB[,2],ylab=expression(paste(alpha)))
-acf(MB[,3],ylab=expression(paste(rho)))
-
-### INTERVALO HPD
-
-require(TeachingDemos)
-
-ICB = emp.hpd(MB[,1],0.95)
-ICA = emp.hpd(MB[,2],0.95)
-ICR = emp.hpd(MB[,3],0.95)
-
-
-
-
-
-
